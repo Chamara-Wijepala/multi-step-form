fca3d9076447496bdbd3b1627a10874ad5242422
Chamara-Wijepala <mr.worf777@gmail.com>
2022-12-28 17:37:19 +0530

    Add background images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Box } from "@mui/material";
+
+import bgSmall from "./assets/images/bg-sm.jpg";
+import bgMedium from "./assets/images/bg-md.jpg";
+import bgLarge from "./assets/images/bg-lg.jpg";
 
 export default function App() {
   return (
     <>
       <CssBaseline />
-      <h1>Hello, World!</h1>
+      <Box
+        sx={{
+          height: "100vh",
+          backgroundImage: {
+            sm: `url(${bgSmall})`,
+            lg: `url(${bgMedium})`,
+            xl: `url(${bgLarge})`,
+          },
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <h1>Hello, World!</h1>
+      </Box>
     </>
   );
 }
