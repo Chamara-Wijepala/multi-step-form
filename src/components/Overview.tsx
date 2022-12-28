fb971f28cd6c15c46086938d21668a101e4edab3
Chamara-Wijepala <mr.worf777@gmail.com>
2022-12-28 22:34:07 +0530

    Add form data to overview step

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,11 +1,68 @@
-import { Box, Button } from "@mui/material";
+import { useState } from "react";
+import { Box, Typography, Button } from "@mui/material";
 
 import { MiddleStep } from "../types";
 
-export default function Overview({ nextStep, previousStep }: MiddleStep) {
+export default function Overview({
+  nextStep,
+  previousStep,
+  formState,
+}: MiddleStep) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const hiddenPassword = formState.password.replace(/./g, "*");
+
   return (
-    <>
-      <div>Overview</div>
+    <Box display="grid" gap="1.5rem">
+      <Box>
+        <Typography variant="h4" component="h2" fontWeight={600}>
+          Finishing up
+        </Typography>
+        <Typography paragraph sx={{ opacity: "0.75" }}>
+          Please make sure everything looks OK before confirming.
+        </Typography>
+      </Box>
+
+      <Box>
+        <Box>
+          <Typography paragraph>
+            <Box component="span" fontWeight={600}>
+              First Name:
+            </Box>{" "}
+            {formState.firstName}
+          </Typography>
+          <Typography paragraph>
+            <Box component="span" fontWeight={600}>
+              Last Name:
+            </Box>{" "}
+            {formState.lastName}
+          </Typography>
+          <Typography paragraph>
+            <Box component="span" fontWeight={600}>
+              Gender:
+            </Box>{" "}
+            {formState.gender}
+          </Typography>
+          <Typography paragraph>
+            <Box component="span" fontWeight={600}>
+              Email:
+            </Box>{" "}
+            {formState.email}
+          </Typography>
+          <Box display="flex" alignItems="center" gap="1.5rem">
+            <Typography paragraph marginBottom={0}>
+              <Box component="span" fontWeight={600}>
+                Password:
+              </Box>{" "}
+              {showPassword ? formState.password : hiddenPassword}
+            </Typography>
+            <Button size="small" onClick={() => setShowPassword(!showPassword)}>
+              Reveal
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+
       <Box display="flex" justifyContent="space-between">
         <Button size="small" onClick={previousStep}>
           Go Back
@@ -20,6 +77,6 @@ export default function Overview({ nextStep, previousStep }: MiddleStep) {
           Confirm
         </Button>
       </Box>
-    </>
+    </Box>
   );
 }
