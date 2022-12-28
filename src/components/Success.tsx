384142307170f4b5718b6498a0d5c6630f0bd5dc
Chamara-Wijepala <mr.worf777@gmail.com>
2022-12-28 22:44:13 +0530

    Add success message

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -1,3 +1,16 @@
+import { Box, Typography } from "@mui/material";
+import { PeopleAlt } from "@mui/icons-material";
+
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <Box sx={{ textAlign: "center", marginBlock: "4rem" }}>
+      <Typography variant="h4" component="h2" sx={{ marginBlock: "1rem" }}>
+        Thank you for signing up!
+      </Typography>
+      <Typography sx={{ opacity: "0.75" }}>
+        We hope you have an amazing experience!
+      </Typography>
+      <PeopleAlt sx={{ fontSize: "8rem", opacity: 0.5 }} />
+    </Box>
+  );
 }
